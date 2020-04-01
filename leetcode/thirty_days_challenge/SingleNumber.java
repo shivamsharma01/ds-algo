0162462071780ae7d35396f36b47257594f3d725
Shivam Sharma <prisonmatch@gmail.com>
2020-04-01 14:47:06 +0530

    leetcode day 1

---
+++ leetcode/thirty_days_challenge/SingleNumber.java
@@ -0,0 +1,11 @@
+package leetcode.thirty_days_challenge;
+
+class Solution {
+    public int singleNumber(int[] nums) {
+        int first = nums[0];
+        for(int i = 1; i < nums.length; i++) {
+            first = first^nums[i];
+        }
+        return first;
+    }
+}
