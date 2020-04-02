306a33c607476ac3610067d7bc7a154c0f20cb65
Shivam Sharma <prisonmatch@gmail.com>
2020-04-02 13:17:54 +0530

    leetcode day 2 happynumber

---
+++ leetcode/thirty_days_challenge/HappyNumber.java
@@ -0,0 +1,36 @@
+package leetcode.thirty_days_challenge;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class HappyNumber {
+	
+	public boolean isHappy(int n) {
+		Set<Integer> set = new HashSet<Integer>();
+		while (true) {
+			if (n == 1) {
+				return true;
+			}
+			if (!set.add(n)) {
+				break;
+			}
+			n = returnSumOfSquareOfDigits(n);
+		}
+		return false;
+	}
+
+	private int returnSumOfSquareOfDigits(int n) {
+		char[] cArr = new String("" + n).toCharArray();
+		int sum = 0;
+		for (char c : cArr) {
+			int a = Character.getNumericValue(c);
+			sum += a * a;
+		}
+		return sum;
+	}
+
+	public static void main(String[] args) {
+		System.out.println(new HappyNumber().isHappy(1000090000));
+	}
+
+}
